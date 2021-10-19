577fac40c16287bab917fbc7bfe47e1b6a55ae1c
Aaron Reynolds <reynolaa@login1.ascent.olcf.ornl.gov>
2021-10-19 14:33:31 -0400

    Added script with nvcc compile command

---
+++ gpu-compile.sh
@@ -0,0 +1,2 @@
+nvcc -O3 -std=c++11 -ccbin mpicxx -I../tpls/mfem-4.3 -I../tpls/hypre/src/hypre/include miniapp.cpp -L../tpls/mfem-4.3 -lmfem -L../tpls/hypre/src/hypre/lib -lHYPRE -lcusparse -lcurand -L../tpls/metis-4.0 -lmetis -lcusparse -lrt -o DeNTapp
+
